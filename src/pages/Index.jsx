87990fb52f10f5c4e003109e73eb28bdab8b68d0
Mahendra Prajapati <sourceforget32@gmail.com>
2026-05-16 03:04:02 +0530

    Remove ScrollVelocity home strip

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -2,7 +2,6 @@ import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import Features from "@/components/Features";
 import WhyMindSupport from "@/components/WhyMindSupport";
-import HomeVelocityStrip from "@/components/HomeVelocityStrip";
 import HomeFlowingMenu from "@/components/HomeFlowingMenu";
 import HomeSupportFlow, { HomeCounsellors } from "@/components/HomeSupportFlow";
 import CallToAction from "@/components/CallToAction";
@@ -11,7 +10,6 @@ const Index = () => {
     return (<div className="min-h-screen bg-background">
       <Navigation />
       <Hero />
-      <HomeVelocityStrip />
       <HomeCounsellors />
       <WhyMindSupport />
       <HomeFlowingMenu />
